1323147fda5b067b1ed467b28166cc93b900dbee
cho45 <cho45@lowreal.net>
2014-02-15 20:07:58 +0900

    i2c devices

---
+++ lib/i2c/device/aqm0802.rb
@@ -0,0 +1,57 @@
+# coding: utf-8
+
+require "i2c"
+require "i2c/device/hd44780"
+
+class AQM0802A < HD44780
+	def initialize(address=0x3e, path=nil)
+		super
+		@is = 0
+	end
+
+	# This device does not support read
+	undef i2cget
+
+	def initialize_lcd
+		function_set(1, 1, 0, 1)
+		internal_osc_frequency(0, 0b100)
+		power_icon_control_contrast_set(0, 1, 0b10000)
+		follower_control(1, 0b100)
+		function_set(1, 1, 0, 0)
+		display_on_off_control(1, 0, 0)
+		clear
+	end
+
+
+	# must set is = 1 before call
+	def internal_osc_frequency(bs, f)
+		raise "is must be 1" unless @is == 1
+		f &= 0b111
+		i2cset(0, 0b00010000 | (bs << 3) | (f))
+		sleep 26.3e-6
+	end
+
+	def power_icon_control_contrast_set(ion, bon, c)
+		c &= 0b111111
+		# contrast_set
+		i2cset(0, 0b01110000 | (c&0b111))
+		sleep 26.3e-6
+		# power_icon_control_contrast_set
+		i2cset(0, 0b01010000 | (ion<<3) | (bon<<2) | (c>>3))
+		sleep 26.3e-6
+	end
+
+	def follower_control(fon, rab)
+		i2cset(0, 0b01100000 | (fon<<3) | rab)
+		sleep 300e-3
+	end
+
+	# is : instruction set 1: extension, 0: normal
+	def function_set(dl, n, f, is)
+		@is = is
+		i2cset(0, 0b00100000 | (dl<<4) | (n<<3) | (f<<2) | (is))
+		sleep 37e-6
+	end
+end
+
+
---
+++ lib/i2c/device/hd44780.rb
@@ -0,0 +1,132 @@
+# coding: utf-8
+
+require "i2c"
+
+# I2C interface with HD44780 compatible commands
+class HD44780 < I2CDevice
+	MAP = Hash[
+		[
+			"｡｢｣､・ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ".split(//).map {|c|
+				c.force_encoding(Encoding::BINARY)
+			},
+			(0b10100001..0b11011111).map {|c|
+				c.chr
+			}
+		].transpose
+	]
+
+	def initialize(address, path=nil)
+		super
+		@lines = []
+		initialize_lcd
+	end
+
+	def initialize_lcd
+		function_set(1, 1, 0)
+		display_on_off_control(1, 0, 0)
+		clear
+	end
+
+	def put_line(line, str, force=false)
+		str.force_encoding(Encoding::BINARY)
+		str.gsub!(/#{MAP.keys.join('|')}/, MAP)
+
+		str = "%- 16s" % str
+
+		if force || str != @lines[line]
+			# set ddram address
+			set_ddram_address(0x40 * line)
+			sleep 53e-6
+			i2cset(*str.unpack("C*").map {|i| [0x80, i] }.flatten)
+			sleep 53e-6
+		end
+		@lines[line] = str
+	end
+
+	# Usage:
+	# lcd.define_character(0, [
+	# 	0,1,1,1,0,
+	# 	1,0,0,0,1,
+	# 	1,1,0,1,1,
+	# 	1,0,1,0,1,
+	# 	1,1,0,1,1,
+	# 	1,0,0,0,1,
+	# 	1,0,0,0,1,
+	# 	0,1,1,1,0,
+	# ])
+	def define_character(n, array)
+		raise "n < 8" unless n < 8
+		raise "array size must be 40 (5x8)" unless array.size == 40
+
+		array = array.each_slice(5).map {|i|
+			i.inject {|r,i| (r << 1) + i }
+		}
+		set_cgram_address(8 * n)
+		sleep 53e-6
+		i2cset(*array.map {|i| [0x80, i] }.flatten)
+		sleep 53e-6
+	end
+
+	def clear
+		@lines.clear
+		clear_display
+	end
+
+	def clear_display
+		i2cset(0, 0b00000001)
+		sleep 2.16e-3
+	end
+
+	def return_home
+		i2cset(0, 0b00000010)
+		sleep 1.52e-3
+	end
+
+	# i_d : increment or decrement: 1: increment, 0: decrement
+	# s   : shift entire display: 1: left, 0: right
+	def entry_mode_set(i_d, s)
+		i2cset(0, 0b00000100 | (i_d<<1) | (s))
+		sleep 37e-6
+	end
+
+	# d: set entire display on/off
+	# c: cursor on/off
+	# b: blink cursor
+	def display_on_off_control(d, c, b)
+		i2cset(0, 0b00001000 | (d<<2) | (c<<1) | (b))
+		sleep 37e-6
+	end
+
+	def cursor_or_display_shift(s_c, r_l)
+		i2cset(0, 0b00010000 | (s_c<<3) | (r_l<<2))
+		sleep 37e-6
+	end
+
+	# dl  data_length: 1: 8bit, 0: 4bit
+	# n   number_of_display_lines: 1: 2-line, 0: 1-line
+	# f   character_font: 1: double font, 0: normal
+	def function_set(dl, n, f)
+		i2cset(0, 0b00100000 | (dl<<4) | (n<<3) | (f<<2))
+		sleep 37e-6
+	end
+
+	def set_cgram_address(address)
+		address = address & 0b0011111
+		i2cset(0, 0b01000000 | address)
+		sleep 37e-6
+	end
+
+	def set_ddram_address(address)
+		address = address & 0b0111111
+		i2cset(0, 0b10000000 | address)
+		sleep 37e-6
+	end
+
+	def read_busy_flag_and_address
+		read = i2cget(0b01000000)
+		{
+			:busy => (read & 0b10000000) != 0,
+			:address_counter => read & 0b01111111
+		}
+	end
+end
